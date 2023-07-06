75de7280eaeca2350c2d00baa22354bbb556e0ea
suzhiwei2000 <83455164+suzhiwei2000@users.noreply.github.com>
2023-07-06 12:54:25 +0800

    修改配置

---
+++ src/routers/index.tsx
@@ -0,0 +1,56 @@
+import { Navigate, useRoutes } from "react-router-dom";
+import { RouteObject } from "@/routers/interface";
+import Login from "@/views/login/index";
+import Home from "@/views/home/index";
+
+// * 导入所有router
+const metaRouters = import.meta.globEager("./modules/*.tsx");
+
+// * 处理路由
+export const routerArray: RouteObject[] = [];
+Object.keys(metaRouters).forEach(item => {
+	Object.keys(metaRouters[item]).forEach((key: any) => {
+		routerArray.push(...metaRouters[item][key]);
+	});
+});
+
+export const rootRouter: RouteObject[] = [
+	// {
+	// 	path: "/",
+	// 	element: <Navigate to="/login" />
+	// },
+	{
+		path: "/",
+		element: <Navigate to="/home" />
+	},
+	{
+		path: "/login",
+		element: <Login />,
+		meta: {
+			requiresAuth: false,
+			title: "登录页",
+			key: "login"
+		}
+	},
+	{
+		path: "/home",
+		element: <Home />,
+		meta: {
+			requiresAuth: false,
+			title: "首页",
+			key: "home"
+		}
+	},
+	...routerArray,
+	{
+		path: "*",
+		element: <Navigate to="/404" />
+	}
+];
+
+const Router = () => {
+	const routes = useRoutes(rootRouter);
+	return routes;
+};
+
+export default Router;
